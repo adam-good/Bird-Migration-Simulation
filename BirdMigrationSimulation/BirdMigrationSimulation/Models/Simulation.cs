c164a2f35910b22c098fdf3f303576d1ec09e553
Adam Good <adam1good2@outlook.com>
2019-08-14 16:06:32 -0500

    Added comments to Simulation

--- BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace BirdMigrationSimulation.Models
 {
+    /// <summary>
+    /// Represents the actual simulation that can be run.
+    /// </summary>
     class Simulation
     {
         public Random Rng { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rng_seed">Optional seed for Random Number Generator. A value of 0 (the default value) will result in a random seed.</param>
         public Simulation(int rng_seed = 0)
         {
             if (rng_seed != 0)
