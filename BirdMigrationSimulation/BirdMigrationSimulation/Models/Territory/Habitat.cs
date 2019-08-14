58d77f3db2c5f2e3f4c8fbf2d5f4df0aa94ad72d
Adam Good <adam1good2@outlook.com>
2019-08-14 16:11:00 -0500

    Added basic fields and constructor for Habitat and Territory classes

--- BirdMigrationSimulation/BirdMigrationSimulation/Models/Territory/Habitat.cs
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Territory/Habitat.cs
@@ -11,5 +11,38 @@ namespace BirdMigrationSimulation.Models.Territory
     /// </summary>
     class Habitat
     {
+        /// <summary>
+        /// The Simulation this habitat belongs to.
+        /// </summary>
+        public Simulation Simulation => Territory.Simulation;
+
+        /// <summary>
+        /// The Territory which this habitat exists in
+        /// </summary>
+        private Territory Territory { get; set; }
+
+        /// <summary>
+        /// A value in the range [0,1] representing how well suited the habitat is to supporting an inhabitant.
+        /// </summary>
+        public double HabitatQualityIndex { get; private set; }
+
+        /// <summary>
+        /// Coordinates to denote the location of the habitat
+        /// </summary>
+        public (int x, int y) Coordinates { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="territory">The Territory object which this habitat will belong to</param>
+        /// <param name="hqi">The Habitat Quality Index for this habitat</param>
+        /// <param name="x">This Habitat's X coordinate</param>
+        /// <param name="y">This Habitat's Y coordinate</param>
+        public Habitat(Territory territory, double hqi, int x, int y)
+        {
+            this.Territory = territory;
+            this.HabitatQualityIndex = hqi;
+            this.Coordinates = (x, y);
+        }
     }
 }
--- BirdMigrationSimulation/BirdMigrationSimulation/Models/Territory/Territory.cs
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Territory/Territory.cs
@@ -8,9 +8,28 @@ namespace BirdMigrationSimulation.Models.Territory
 {
     /// <summary>
     /// This represents the land that birds can move around
-    /// It is implemented as a list of habitats
+    /// It is implemented as a a wrapper around a list of habitats
     /// </summary>
     class Territory
     {
+        /// <summary>
+        /// The Simulation this Territory belongs to
+        /// </summary>
+        public Simulation Simulation { get; private set; }
+
+        /// <summary>
+        /// A collection of the habitats in this territory.
+        /// Indexing is spacially irrelevant to the habitat location.
+        /// </summary>
+        public List<Habitat> Habitats { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="simulation">The simulation this territory will belong to</param>
+        public Territory(Simulation simulation)
+        {
+            this.Simulation = simulation;
+        }
     }
 }
