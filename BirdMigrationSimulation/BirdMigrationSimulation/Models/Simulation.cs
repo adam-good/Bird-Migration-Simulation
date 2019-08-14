61b791c7ee8ed68c92e3092f7aaeb238368cd5f6
Adam Good <adam1good2@outlook.com>
2019-08-14 14:22:11 -0500

    Added Basic Simulation Class

---
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdMigrationSimulation.Models
+{
+    class Simulation
+    {
+        public Random Rng = new Random();
+
+        public Simulation(int rng_seed = 0)
+        {
+            if (rng_seed != 0)
+                Rng = new Random(rng_seed);
+        }
+
+        public void Run() => throw new NotImplementedException();
+
+        public void Stop() => throw new NotImplementedException();
+    }
+}
