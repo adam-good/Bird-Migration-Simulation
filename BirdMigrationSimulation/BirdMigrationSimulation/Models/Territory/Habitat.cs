2829d5a3d6440714ae344f320ad75b42ec5f001d
Adam Good <adam1good2@outlook.com>
2019-08-14 14:51:52 -0500

    Added Territory and Habitat Classes

---
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Territory/Habitat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdMigrationSimulation.Models.Territory
+{
+    /// <summary>
+    /// This class represents a section of land that can support a single bird nest
+    /// </summary>
+    class Habitat
+    {
+    }
+}
---
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Territory/Territory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdMigrationSimulation.Models.Territory
+{
+    /// <summary>
+    /// This represents the land that birds can move around
+    /// It is implemented as a list of habitats
+    /// </summary>
+    class Territory
+    {
+    }
+}
