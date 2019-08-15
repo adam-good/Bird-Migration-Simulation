b05beb45bd462ff5d0598d3e8d3d51a196e1f7ff
Adam Good <adam1good2@outlook.com>
2019-08-15 16:28:54 -0500

    Added basic Init() method and Territory to Simulation

--- BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BirdMigrationSimulation.Models.Area;
+
 namespace BirdMigrationSimulation.Models
 {
     /// <summary>
@@ -13,6 +15,8 @@ namespace BirdMigrationSimulation.Models
     {
         public Random Rng { get; private set; }
 
+        private Territory Territory { get; set; }
+
         /// <summary>
         /// 
         /// </summary>
@@ -21,6 +25,14 @@ namespace BirdMigrationSimulation.Models
         {
             if (rng_seed != 0)
                 Rng = new Random(rng_seed);
+
+            (int x, int y) = (64, 64); // This is temporary. These should come from a configuration file
+            Init(x, y);
+        }
+
+        public void Init(int width, int height)
+        {
+            this.Territory = new Territory(this, width, height);
         }
 
         public void Run() => throw new NotImplementedException();
