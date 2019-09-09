17d93e2c56b60a0211f1c568389b3b8ea8621f07
Adam Good <dev.adamgood@gmail.com>
2019-09-09 16:51:41 +0000

    State management

--- BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/Simulation.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using BirdMigrationSimulation.Models.Area;
-using BirdMigrationSimulation.Models.Data;
 using BirdMigrationSimulation.Models.Inhabitants;
+using BirdMigrationSimulation.Utilities;
 
 namespace BirdMigrationSimulation.Models
 {
@@ -19,7 +20,7 @@ namespace BirdMigrationSimulation.Models
         public Territory Territory { get; set; }
         public Population Population { get; set; }
 
-        public DataManager DataManager { get; private set; }
+        private SimulationStateManager StateManager { get; set; }
 
         /// <summary>
         /// 
@@ -32,7 +33,7 @@ namespace BirdMigrationSimulation.Models
             if (rng_seed != 0)
                 Rng = new Random(rng_seed);
 
-            this.DataManager = new DataManager(this);
+            this.StateManager = new SimulationStateManager("C:/Users/ajgood/Desktop/Birdies/", this);
 
 
             // This is temporary. These should come from a configuration file
@@ -42,7 +43,7 @@ namespace BirdMigrationSimulation.Models
 
         public void LoadState(int timestep)
         {
-            DataManager.LoadState(timestep);
+            StateManager.LoadState(timestep);
         }
 
         /// <summary>
@@ -75,10 +76,158 @@ namespace BirdMigrationSimulation.Models
                 Population.MigrateBirds(Population.Birds);
 
                 if (i % checkpointStep == 0)
-                    DataManager.SaveState(i);
+                    StateManager.SaveState(i);
             }
         }
 
         public void Stop() => throw new NotImplementedException();
+
+        private class SimulationStateManager
+        {
+            public string RootDirectory { get; private set; }
+            public Simulation Simulation { get; private set; }
+            private Territory Territory => Simulation.Territory;
+            private Population Population => Simulation.Population;
+            private Random Random => Simulation.Rng;
+
+            public SimulationStateManager(string rootDir, Simulation simulation)
+            {
+                this.RootDirectory = rootDir;
+                this.Simulation = simulation;
+            }
+
+            public void SaveState(int timestep)
+            {
+                string territoryFilePath = $"{RootDirectory}territory_{timestep}.csv";
+                string populationFilePath = $"{RootDirectory}population_{timestep}.csv";
+                string rngFilePath = $"{RootDirectory}randomstate_{timestep}.bin";
+
+                SaveTerritory(territoryFilePath);
+                SavePopulation(populationFilePath);
+                SaveRngState(rngFilePath);
+
+                Console.WriteLine($"Checkpoint {timestep} Done!");
+            }
+
+            public void LoadState(int timestep)
+            {
+                string territoryFilePath = $"{RootDirectory}territory_{timestep}.csv";
+                string populationFilePath = $"{RootDirectory}population_{timestep}.csv";
+                string rngFilePath = $"{RootDirectory}randomstate_{timestep}.bin";
+
+                LoadTerritory(territoryFilePath);
+                LoadPopulation(populationFilePath);
+                LoadRngState(rngFilePath);
+            }
+
+            private void SaveTerritory(string territoryFilePath)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (var habitat in Territory.HabitatGrid)
+                    stringBuilder.Append($"{habitat.Id}, {habitat.Coordinates.x}, {habitat.Coordinates.y}, {habitat.HabitatQualityIndex}\n");
+
+                FileStream file = new FileStream(territoryFilePath, FileMode.Create, FileAccess.Write);
+                using (StreamWriter writer = new StreamWriter(file))
+                    writer.WriteLine(stringBuilder.ToString());
+            }
+
+            private void SavePopulation(string populationFilePath)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                foreach (var bird in Population.Birds)
+                    stringBuilder.Append($"{bird.birdId}, {bird.CurrentHabitat.Id}, {bird.Sex}, {bird.Age}\n");
+
+                FileStream fileStream = new FileStream(populationFilePath, FileMode.Create, FileAccess.Write);
+                using (StreamWriter writer = new StreamWriter(fileStream))
+                    writer.WriteLine(stringBuilder.ToString());
+            }
+
+            private void SaveRngState(string rngFilePath)
+            {
+                RandomState state = Random.Save();
+
+                FileStream stream = new FileStream(rngFilePath, FileMode.Create, FileAccess.Write);
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    foreach (var item in state.State)
+                        writer.Write(item);
+                }
+            }
+
+            private void LoadTerritory(string territroyFilePath)
+            {
+                List<Habitat> habitatList = new List<Habitat>();
+                using (var reader = new StreamReader(territroyFilePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        // Since there is one empty line at the end of the file this avoids it
+                        if (line == String.Empty) continue;
+                        var values = line.Split(',');
+
+                        (int id, int x, int y, double hqi) = (Int32.Parse(values[0]),
+                                                      Int32.Parse(values[1]),
+                                                      Int32.Parse(values[2]),
+                                                      Double.Parse(values[3])
+                                          );
+                        Habitat habitat = new Habitat(Territory, hqi, x, y, id);
+                        habitatList.Add(habitat);
+                    }
+                }
+
+                Territory territory = new Territory(this.Simulation, habitatList);
+                this.Simulation.Territory = territory;
+            }
+
+            private void LoadPopulation(string populationFilePath)
+            {
+                List<Inhabitant> inhabitants = new List<Inhabitant>();
+
+                using (var reader = new StreamReader(populationFilePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        // Since there is one empty line at the end of the file this avoids it
+                        if (line == String.Empty)
+                            continue;
+                        var values = line.Split(',');
+
+                        (int birdId, int habitatId, Sex sex, Age age) = (
+                                Int32.Parse(values[0]),
+                                Int32.Parse(values[1]),
+                                (Sex)Enum.Parse(typeof(Sex), values[2], true),
+                                (Age)Enum.Parse(typeof(Age), values[3], true)
+                            );
+
+                        Bird bird = new Bird(this.Population, sex, age, birdId);
+                        Habitat habitat = Territory.HabitatGrid.Where(h => h.Id == habitatId).First();
+                        Simulation.InsertInhabitant(bird, habitat);
+                        inhabitants.Add(bird);
+                    }
+                }
+
+                Population population = new Population(this.Simulation, inhabitants);
+                this.Simulation.Population = population;
+            }
+
+            private void LoadRngState(string rngFilePath)
+            {
+                RandomState state;
+
+                FileStream stream = new FileStream(rngFilePath, FileMode.Open, FileAccess.Read);
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    int count = (int)reader.BaseStream.Length;
+                    byte[] bytes = reader.ReadBytes(count);
+                    state = new RandomState(bytes);
+                }
+
+                Random random = state.Restore();
+                this.Simulation.Rng = random;
+            }
+        }
     }
 }
