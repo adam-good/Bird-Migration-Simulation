60ce9df99878e0611388304c24dc2b61c46dc20b
Adam Good <adam1good2@gmail.com>
2020-02-19 12:11:51 -0600

    Added a constant distribution

---
+++ BirdMigrationSimulation/BirdMigrationSimulation/Models/RNG/ConstantDistribution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdMigrationSimulation.Models.RNG
+{
+    public class ConstantDistribution
+    {
+        private Random sourceRandom;
+
+        public ConstantDistribution(Random source)
+        {
+            this.sourceRandom = source;
+        }
+
+        public double Sample(double a)
+        {
+            return a;
+        }
+
+    }
+}
